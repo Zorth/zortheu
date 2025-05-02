0197ba8e1c8afbf73a155879990db2fb452264a4
Jasper Goens <jasper_goens@hotmail.com>
2025-05-02 21:58:57 +0200

    fix BrowserRouter

--- src/app.rs
+++ src/app.rs
@@ -1,18 +1,37 @@
 use yew::prelude::*;
+use yew_router::prelude::*;
 
 use crate::components::navelement::NavElement;
 
 use gloo::console::log;
 
+#[derive(Debug, Clone, Copy, PartialEq, Routable)]
+enum Route {
+    #[at("/")]
+    Home,
+    #[not_found]
+    #[at("/404")]
+    NotFound,
+}
+
+fn switch(routes: Route) -> Html {
+    match routes {
+        Route::Home => html! { <h1>{ "Home" }</h1> },
+        Route::NotFound => html! { <h1>{ "404" }</h1> },
+    }
+}
+
 #[function_component]
 pub fn App() -> Html {
-
     log!("Example console log");
 
     html! {
         <main>
-            <h1>{ "Hello World!!" }</h1>
             <NavElement />
-        </main>
+            <BrowserRouter>
+
+            <Switch<Route> render={switch} />
+            </BrowserRouter>
+            </main>
     }
 }
