b47216e150d095174ce2bd0e51b86ab56ba1d647
Jasper Goens <jasper_goens@hotmail.com>
2025-05-02 14:06:09 +0200

    add log example

--- src/app.rs
+++ src/app.rs
@@ -1,7 +1,11 @@
+use gloo::console::log;
 use yew::prelude::*;
 
 #[function_component(App)]
 pub fn app() -> Html {
+
+    log!("Example console log");
+
     html! {
         <main>
             <h1>{ "Hello World!" }</h1>
