8254c01fa71fd8e72fee30dd5d34a80d415d2136
Jasper Goens <jasper_goens@hotmail.com>
2025-05-06 16:13:17 +0200

    outline base st page

--- src/pages/space_trade.rs
+++ src/pages/space_trade.rs
@@ -3,9 +3,24 @@ use yew::prelude::*;
 
 #[function_component]
 pub fn SpaceTrader() -> Html {
-    html!{
-        <h1>
-        {"This is space traders"}
-        </h1>
+
+    return html!{
+        <div class={classes!{"st"}}>
+            <div class={classes!{"st-header"}}>
+            </div>
+            <div class={classes!{"st-body"}}>
+                <div class={classes!{"st-left-sidebar"}}>
+                    <div class={classes!{"st-sidebar-comp"}}></div>
+                    <div class={classes!{"st-sidebar-comp"}}></div>
+                    <div class={classes!{"st-sidebar-comp"}}></div>
+                </div>
+                <div class={classes!{"st-main"}}>
+                    <div class={classes!{"st-main-top"}}>
+                    </div>
+                    <div class={classes!{"st-main-bot"}}>
+                    </div>
+                </div>
+            </div>
+        </div>
     }
 }
